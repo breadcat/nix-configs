01eb1437638dd09a0f5b19713e3984ae751d975d
breadcat <breadcat@users.noreply.github.com>
2025-07-28 14:28:11 +0100

    Use a few variables for easier edits

--- common/locale.nix
+++ common/locale.nix
@@ -1,16 +1,22 @@
+{ config, pkgs, ... }:
+
+let
+  timezone = "Europe/London";
+  locale = "en_GB.UTF-8";
+in
 {
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
+  time.timeZone = timezone;
+  i18n.defaultLocale = locale;
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
+    LC_ADDRESS = locale;
+    LC_IDENTIFICATION = locale;
+    LC_MEASUREMENT = locale;
+    LC_MONETARY = locale;
+    LC_NAME = locale;
+    LC_NUMERIC = locale;
+    LC_PAPER = locale;
+    LC_TELEPHONE = locale;
+    LC_TIME = locale;
   };
   services.xserver.xkb.layout = "gb";
   console.keyMap = "uk";
