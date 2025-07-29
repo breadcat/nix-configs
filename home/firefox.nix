5f5d6a2ce83572db4509b4972db3493eedc8dc90
breadcat <breadcat@users.noreply.github.com>
2025-07-29 16:18:25 +0100

    Latest firefox set of settings

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,26 +1,24 @@
 {
 programs.firefox = {
   enable = true;
-
   profiles.default = {
     id = 0;
     name = "default";
-
-    # Example Firefox preferences
     settings = {
       "browser.aboutConfig.showWarning" = false;
-      "browser.gesture.swipe.left" = "";
-      "browser.gesture.swipe.right" = "";
+      "browser.gesture.swipe.left" = "cmd_scrollLeft";
+      "browser.gesture.swipe.right" = "cmd_scrollRight";
       "browser.startup.homepage" = "https://breadcat.github.io/startpage/";
       "browser.theme.content-theme" = "0"; # Dark theme
       "browser.theme.toolbar-theme" = "0"; # Dark theme
+      "browser.toolbars.bookmarks.visibility" = "never";
+      "extensions.pocket.enabled" = false;
+      "general:autoScroll" = true;
       "layout.css.prefers-color-scheme.content-override" = "0"; # Dark CSS themes
       "network.cookie.cookieBehavior" = 1; # Block third-party cookies
       "privacy.donottrackheader.enabled" = true;
     };
     extensions = [
-      # "ublock-origin@raymondhill.net"  # Just an example (must match extension ID)
-      # "uBlock0@raymondhill.net" # uBlock Origin
     ];
   };
 };
