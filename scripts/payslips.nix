aaaa02ca6c638f63dd21eb48adc400ada3436cab
breadcat <breadcat@users.noreply.github.com>
2025-07-30 13:03:02 +0100

    Fix unescaped quotes
    Oops

--- scripts/payslips.nix
+++ scripts/payslips.nix
@@ -4,7 +4,7 @@ let
   payslips = pkgs.writeShellScriptBin "payslips" ''
     # Default destination directory, or use argument
     destination_default="/mnt/paperwork/personal/workplace/wages"
-    destination_dir="${1:-$destination_default}"
+    destination_dir="''${1:-$destination_default}"
 
     # Process matching files
     for file in ./*-[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].pdf; do
@@ -12,10 +12,10 @@ let
         filename=$(basename "$file")
         # Extract the date
         if [[ "$filename" =~ ([0-9]{2})-([0-9]{2})-([0-9]{4})\.pdf$ ]]; then
-            day="${BASH_REMATCH[1]}"
-            month="${BASH_REMATCH[2]}"
-            year="${BASH_REMATCH[3]}"
-            new_filename="${year}-${month}-${day}.pdf"
+            day="''${BASH_REMATCH[1]}"
+            month="''${BASH_REMATCH[2]}"
+            year="''${BASH_REMATCH[3]}"
+            new_filename="''${year}-''${month}-''${day}.pdf"
             mv "$file" "$destination_dir/$new_filename"
             echo "Moved: $file to $destination_dir/$new_filename"
         else
