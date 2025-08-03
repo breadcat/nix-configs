88ba72fe278d306e89ec68de4eb797984141b472
breadcat <breadcat@users.noreply.github.com>
2025-08-03 15:54:30 +0100

    Add fail2ban service

--- common/ssh.nix
+++ common/ssh.nix
@@ -1,11 +1,14 @@
 { username, sshkey, ... }:
 
 {
+  # SSH service
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     };
-
-  # SSH key
+  # Fail2ban service
+  services.fail2ban.enable = true;
+  # Import SSH key
   users.users.${username}.openssh.authorizedKeys.keys = [ "${sshkey}" ];
+
 }
