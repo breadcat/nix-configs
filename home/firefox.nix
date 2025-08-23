75a0775ad8bd521bc20ab9b31ee5114967125e1e
breadcat <breadcat@users.noreply.github.com>
2025-08-23 08:47:14 +0100

    Incomplete firefox config
    Addons work at least!

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,28 +1,54 @@
+{ config, pkgs, ... }:
+
+let
+  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; };
+in
 {
-programs.firefox = {
-  enable = true;
-  profiles.default = {
-    id = 0;
-    name = "default";
-    settings = {
-      "browser.aboutConfig.showWarning" = false;
-      "browser.gesture.swipe.left" = "cmd_scrollLeft";
-      "browser.gesture.swipe.right" = "cmd_scrollRight";
-      "browser.startup.homepage" = "https://breadcat.github.io/startpage/";
-      "browser.theme.content-theme" = "0"; # Dark theme
-      "browser.theme.toolbar-theme" = "0"; # Dark theme
-      "browser.toolbars.bookmarks.visibility" = "never";
-      "extensions.pocket.enabled" = false;
-      "general:autoScroll" = true;
-      "layout.css.prefers-color-scheme.content-override" = "0"; # Dark CSS themes
-      "network.cookie.cookieBehavior" = 1; # Block third-party cookies
-      "privacy.donottrackheader.enabled" = true;
+  # Enable Firefox via Home Manager
+  programs.firefox = {
+    enable = true;
+    languagePacks = [ "en-GB" ];
+
+    profiles.default = {
+      name = "default";
+      isDefault = true;
+
+      extensions.packages = with nur.repos.rycee.firefox-addons; [
+        ublock-origin
+        cookie-autodelete
+        new-tab-override
+      ];
+
+      settings = {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.gesture.swipe.left" = "cmd_scrollLeft";
+        "browser.gesture.swipe.right" = "cmd_scrollRight";
+        "browser.startup.homepage" = "https://breadcat.github.io/startpage/";
+        "browser.theme.content-theme" = "0"; # Dark theme
+        "browser.theme.toolbar-theme" = "0"; # Dark theme
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "extensions.pocket.enabled" = false;
+        "general:autoScroll" = true;
+        "layout.css.prefers-color-scheme.content-override" = "0"; # Dark CSS themes
+        "network.cookie.cookieBehavior" = 1; # Block third-party cookies
+        "privacy.donottrackheader.enabled" = true;
+      };
     };
-    extensions = [
-    ];
+
+    policies = {
+      DisableTelemetry = true;
+      DisablePocket = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
   };
-};
+
 home.sessionVariables = {
+	BROWSER = "firefox";
 	MOZ_ENABLE_WAYLAND = 1;
 };
 }
