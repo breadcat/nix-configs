00a68c3e9eb27993c6d7c1d9f1b294bcc3f6c8cf
breadcat <breadcat@users.noreply.github.com>
2025-08-24 12:50:28 +0100

    Disable all entries until needed later

--- common/emulators.nix
+++ common/emulators.nix
@@ -2,26 +2,31 @@
 {
   environment.systemPackages = with pkgs; [
     # Emulators
-    blastem
-    cemu
-    dolphin-emu
-    duckstation
-    flycast
-    gopher64
-    melonDS
-    mgba
-    pcsx2
-    ppsspp
-    punes
-    rpcs3
-    sameboy
-    snes9x
-    xemu
+    # blastem
+    # cemu
+    # dolphin-emu
+    # duckstation
+    # flycast
+    # gopher64
+    # melonDS
+    # mesen
+    # mgba
+    # pcsx2
+    # ppsspp
+    # rpcs3
+    # sameboy
+    # snes9x
+    # xemu
     # Game engines
-    corsix-th
-    eduke32
-    openra
-    openrct2
-    openttd
+    # corsix-th
+    # eduke32
+    # openra
+    # openrct2
+    # openttd
+    # Frontend
+    # pegasus-frontend
+    # Tools
+    # mame-tools
+    # moonlight-qt
   ];
 }
