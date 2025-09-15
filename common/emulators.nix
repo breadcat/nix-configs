59bbafbfe5fea647650c2b2b01a85922aa9256a9
breadcat <breadcat@users.noreply.github.com>
2025-09-15 14:43:09 +0100

    Looks like we're switching to retroarch

--- common/emulators.nix
+++ common/emulators.nix
@@ -1,32 +1,33 @@
 { pkgs, ... }:
+
+let
+  retroarchWithCores = (
+    pkgs.retroarch.withCores (
+      cores: with cores; [
+      beetle-saturn
+      # cemu
+      dolphin
+      flycast
+      genesis-plus-gx
+      # gopher64
+      melonds
+      mesen
+      mgba
+      parallel-n64
+      pcsx2
+      ppsspp
+      # rpcs3
+      sameboy
+      snes9x
+      swanstation
+      # xemu
+      # Game engines: corsix-th, eduke32, openra, openrct2, openttd
+      # Frontends: pegasus-frontend
+      # Tools: mame-tools, moonlight-qt
+      ]
+    )
+  );
+in
 {
-  environment.systemPackages = with pkgs; [
-    # Emulators
-    # blastem
-    # cemu
-    # dolphin-emu
-    # duckstation
-    # flycast
-    # gopher64
-    # melonDS
-    # mesen
-    # mgba
-    # pcsx2
-    # ppsspp
-    # rpcs3
-    # sameboy
-    # snes9x
-    # xemu
-    # Game engines
-    # corsix-th
-    # eduke32
-    # openra
-    # openrct2
-    # openttd
-    # Frontend
-    # pegasus-frontend
-    # Tools
-    # mame-tools
-    # moonlight-qt
-  ];
+  environment.systemPackages = [ retroarchWithCores ];
 }
