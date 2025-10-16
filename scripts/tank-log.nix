575eda8dd8880dc8e57325ed586dc78f3bae8c4b
breadcat <breadcat@users.noreply.github.com>
2025-10-16 19:28:43 +0100

    Alter indentation
    All working peachily now

--- scripts/tank-log.nix
+++ scripts/tank-log.nix
@@ -2,46 +2,47 @@
 
 let
   tank-log = pkgs.writeShellScriptBin "tank-log" ''
-	# variables
-	git_directory="$HOME/vault/src/logger/"
-	file_git_log="$git_directory/media.log"
-	log_remote="nas:"
-	git_logger="git --git-dir=$git_directory/.git --work-tree=$git_directory"
-	# git configuruation
-	if [ ! -e "$git_directory" ]; then
-		printf "Logger directory not found, quitting...\n"
-		exit 1
-	fi
-	if [ ! -e "$git_directory/.git" ]; then
-		printf "Initialising blank git repo...\n"
-		$git_logger init
-	fi
-	if [ -e "$file_git_log.xz" ]; then
-		printf "Decompressing existing xz archive...\n"
-		xz -d "$file_git_log.xz"
-	fi
-	if [ -e "$file_git_log" ]; then
-		printf "Removing existing log file...\n"
-		rm "$file_git_log"
-	fi
-	printf "Creating log...\n"
-	${pkgs.rclone}/bin/rclone ls "$log_remote" | sort -k2 >"$file_git_log"
-	printf "Appending size information...\n"
-	${pkgs.rclone}/bin/rclone size "$log_remote" >>"$file_git_log"
-	printf "Commiting log file to repository...\n"
-	$git_logger add "$file_git_log"
-	$git_logger commit -m "Update: $(date +%F)"
-	if [ -e "$file_git_log.xz" ]; then
-		printf "Removing xz archive...\n"
-		rm "$file_git_log.xz"
-	fi
-	printf "Compressing log file...\n"
-	xz "$file_git_log"
-	printf "Compressing repository...\n"
-	$git_logger config pack.windowMemory 10m
-	$git_logger config pack.packSizeLimit 20m
-	$git_logger gc --aggressive --prune
-	printf "Log complete!\n"
+  # variables
+  export LC_ALL=C
+  git_directory="$HOME/vault/src/logger/"
+  file_git_log="$git_directory/media.log"
+  log_remote="nas:"
+  git_logger="git --git-dir=$git_directory/.git --work-tree=$git_directory"
+  # git configuruation
+  if [ ! -e "$git_directory" ]; then
+    printf "Logger directory not found, quitting...\n"
+    exit 1
+  fi
+  if [ ! -e "$git_directory/.git" ]; then
+    printf "Initialising blank git repo...\n"
+    $git_logger init
+  fi
+  if [ -e "$file_git_log.xz" ]; then
+    printf "Decompressing existing xz archive...\n"
+    xz -d "$file_git_log.xz"
+  fi
+  if [ -e "$file_git_log" ]; then
+    printf "Removing existing log file...\n"
+    rm "$file_git_log"
+  fi
+  printf "Creating log...\n"
+  ${pkgs.rclone}/bin/rclone ls "$log_remote" | sort -k2 >"$file_git_log"
+  printf "Appending size information...\n"
+  ${pkgs.rclone}/bin/rclone size "$log_remote" >>"$file_git_log"
+  printf "Commiting log file to repository...\n"
+  $git_logger add "$file_git_log"
+  $git_logger commit -m "Update: $(date +%F)"
+  if [ -e "$file_git_log.xz" ]; then
+    printf "Removing xz archive...\n"
+    rm "$file_git_log.xz"
+  fi
+  printf "Compressing log file...\n"
+  xz "$file_git_log"
+  printf "Compressing repository...\n"
+  $git_logger config pack.windowMemory 10m
+  $git_logger config pack.packSizeLimit 20m
+  $git_logger gc --aggressive --prune
+  printf "Log complete!\n"
   '';
 in {
   environment.systemPackages = [tank-log];
