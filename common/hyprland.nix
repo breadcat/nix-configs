d2b17cea233356fd49d9f3c63c7b758f7d98b6d4
Peter <peter@minskio.co.uk>
2025-10-25 09:42:19 +0100

    Add XDG handling to Hyprland include

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -13,4 +13,10 @@
   programs.hyprland.enable = true;
   users.users.${username}.extraGroups = ["seat" "video"];
   services.seatd.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    };
+
 }
