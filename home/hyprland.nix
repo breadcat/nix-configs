edef4c580ec96c2e40c9fe59f6f0998875d8d8be
Peter <peter@minskio.co.uk>
2025-12-23 17:32:17 +0000

    Indentation fix

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -57,8 +57,8 @@
           "linear,0,0,1,1"
           "almostLinear,0.5,0.5,0.75,1.0"
           "quick,0.15,0,0.1,1"
-        ];
-        animation = [
+          ];
+          animation = [
           "global, 1, 10, default"
           "border, 1, 5.39, easeOutQuint"
           "windows, 1, 4.79, easeOutQuint"
@@ -75,122 +75,122 @@
           "workspaces, 1, 1.94, almostLinear, fade"
           "workspacesIn, 1, 1.21, almostLinear, fade"
           "workspacesOut, 1, 1.94, almostLinear, fade"
-        ];
-      };
+          ];
+          };
 
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-        workspace = [
+          dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+          workspace = [
           "w[tv1], gapsout:0, gapsin:0"
           "f[1], gapsout:0, gapsin:0"
-        ];
-        windowrulev2 = [
+          ];
+          windowrulev2 = [
           "bordersize 0, floating:0, onworkspace:w[tv1]"
           "rounding 0, floating:0, onworkspace:w[tv1]"
           "bordersize 0, floating:0, onworkspace:f[1]"
           "rounding 0, floating:0, onworkspace:f[1]"
+          ];
+          };
+
+          master = {
+          new_status = "master";
+        };
+
+        misc = {
+          force_default_wallpaper = 0;
+          disable_hyprland_logo = true;
+        };
+
+        input = {
+          kb_layout = "gb";
+          kb_options = "caps:backspace";
+          follow_mouse = 1;
+          sensitivity = 0;
+          touchpad = {
+            natural_scroll = false;
+          };
+        };
+
+        bind = [
+          "SUPER, R, exec, tofi-run | xargs hyprctl dispatch exec --"
+          "SUPER, W, exec, firefox"
+          "SUPER, T, exec, alacritty"
+          "SUPER_SHIFT, W, exec, firefox -private-window"
+          "ALT, Tab, cyclenext"
+          "ALT SHIFT, Tab, cyclenext, prev"
+          "SUPER, RETURN, exec, alacritty"
+          "SUPER_SHIFT, Q, killactive,"
+          "SUPER, X, exit,"
+          "SUPER, E, exec, alacritty -e lf"
+          "SUPER, Space, togglefloating,"
+          "SUPER, F, fullscreen,"
+          "SUPER, P, pseudo,"
+          "SUPER, J, togglesplit,"
+          "SUPER, left, movefocus, l"
+          "SUPER, right, movefocus, r"
+          "SUPER, up, movefocus, u"
+          "SUPER, down, movefocus, d"
+          "SUPER, 1, workspace, 1"
+          "SUPER, 2, workspace, 2"
+          "SUPER, 3, workspace, 3"
+          "SUPER, 4, workspace, 4"
+          "SUPER, 5, workspace, 5"
+          "SUPER, 6, workspace, 6"
+          "SUPER, 7, workspace, 7"
+          "SUPER, 8, workspace, 8"
+          "SUPER, 9, workspace, 9"
+          "SUPER, 0, workspace, 10"
+          "SUPER SHIFT, 1, movetoworkspace, 1"
+          "SUPER SHIFT, 2, movetoworkspace, 2"
+          "SUPER SHIFT, 3, movetoworkspace, 3"
+          "SUPER SHIFT, 4, movetoworkspace, 4"
+          "SUPER SHIFT, 5, movetoworkspace, 5"
+          "SUPER SHIFT, 6, movetoworkspace, 6"
+          "SUPER SHIFT, 7, movetoworkspace, 7"
+          "SUPER SHIFT, 8, movetoworkspace, 8"
+          "SUPER SHIFT, 9, movetoworkspace, 9"
+          "SUPER SHIFT, 0, movetoworkspace, 10"
+          "SUPER, mouse_down, workspace, e+1"
+          "SUPER, mouse_up, workspace, e-1"
+        ];
+
+        bindm = [
+          "ALT, mouse:272, movewindow"
+          "ALT, mouse:273, resizewindow"
+        ];
+
+        bindel = [
+          ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+          ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+        ];
+
+        bindl = [
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"
+        ];
+
+        windowrulev2 = [
+          "suppressevent maximize, class:.*"
+          "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
         ];
       };
-
-      master = {
-        new_status = "master";
-      };
-
-      misc = {
-        force_default_wallpaper = 0;
-        disable_hyprland_logo = true;
-      };
-
-      input = {
-        kb_layout = "gb";
-        kb_options = "caps:backspace";
-        follow_mouse = 1;
-        sensitivity = 0;
-        touchpad = {
-          natural_scroll = false;
-        };
-      };
-
-      bind = [
-        "SUPER, R, exec, tofi-run | xargs hyprctl dispatch exec --"
-        "SUPER, W, exec, firefox"
-        "SUPER, T, exec, alacritty"
-        "SUPER_SHIFT, W, exec, firefox -private-window"
-        "ALT, Tab, cyclenext"
-        "ALT SHIFT, Tab, cyclenext, prev"
-        "SUPER, RETURN, exec, alacritty"
-        "SUPER_SHIFT, Q, killactive,"
-        "SUPER, X, exit,"
-        "SUPER, E, exec, alacritty -e lf"
-        "SUPER, Space, togglefloating,"
-        "SUPER, F, fullscreen,"
-        "SUPER, P, pseudo,"
-        "SUPER, J, togglesplit,"
-        "SUPER, left, movefocus, l"
-        "SUPER, right, movefocus, r"
-        "SUPER, up, movefocus, u"
-        "SUPER, down, movefocus, d"
-        "SUPER, 1, workspace, 1"
-        "SUPER, 2, workspace, 2"
-        "SUPER, 3, workspace, 3"
-        "SUPER, 4, workspace, 4"
-        "SUPER, 5, workspace, 5"
-        "SUPER, 6, workspace, 6"
-        "SUPER, 7, workspace, 7"
-        "SUPER, 8, workspace, 8"
-        "SUPER, 9, workspace, 9"
-        "SUPER, 0, workspace, 10"
-        "SUPER SHIFT, 1, movetoworkspace, 1"
-        "SUPER SHIFT, 2, movetoworkspace, 2"
-        "SUPER SHIFT, 3, movetoworkspace, 3"
-        "SUPER SHIFT, 4, movetoworkspace, 4"
-        "SUPER SHIFT, 5, movetoworkspace, 5"
-        "SUPER SHIFT, 6, movetoworkspace, 6"
-        "SUPER SHIFT, 7, movetoworkspace, 7"
-        "SUPER SHIFT, 8, movetoworkspace, 8"
-        "SUPER SHIFT, 9, movetoworkspace, 9"
-        "SUPER SHIFT, 0, movetoworkspace, 10"
-        "SUPER, mouse_down, workspace, e+1"
-        "SUPER, mouse_up, workspace, e-1"
-      ];
-
-      bindm = [
-        "ALT, mouse:272, movewindow"
-        "ALT, mouse:273, resizewindow"
-      ];
-
-      bindel = [
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
-      ];
-
-      bindl = [
-        ", XF86AudioNext, exec, playerctl next"
-        ", XF86AudioPause, exec, playerctl play-pause"
-        ", XF86AudioPlay, exec, playerctl play-pause"
-        ", XF86AudioPrev, exec, playerctl previous"
-      ];
-
-      windowrulev2 = [
-        "suppressevent maximize, class:.*"
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
     };
-  };
 
-  programs.fish = {
-    interactiveShellInit = ''
-      set -gx HYPRPATH (whereis Hyprland | awk '{print $NF}')
-      if test (tty) = "/dev/tty1"; exec $HYPRPATH; end
-    '';
-  };
+    programs.fish = {
+      interactiveShellInit = ''
+        set -gx HYPRPATH (whereis Hyprland | awk '{print $NF}')
+        if test (tty) = "/dev/tty1"; exec $HYPRPATH; end
+      '';
+    };
 
-  dconf = {
-    enable = true;
-  };
-}
+    dconf = {
+      enable = true;
+    };
+  }
