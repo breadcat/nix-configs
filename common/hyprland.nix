22a9d729b0d7fa09e0c9b55ef1002e8cdd80181e
Peter <peter@minskio.co.uk>
2025-12-23 17:28:49 +0000

    Fix XDG once and for all

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -9,14 +9,14 @@
     swayimg
     tofi
     wl-clipboard
-    ];
+  ];
   programs.hyprland.enable = true;
   users.users.${username}.extraGroups = ["seat" "video"];
   services.seatd.enable = true;
 
   xdg.portal = {
     enable = true;
-    xdgOpenUsePortal = true;
+    config = {
     };
 
 }
