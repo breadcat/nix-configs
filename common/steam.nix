238042dae31ca8ded590e70ace7cd86c9717f2c7
Peter <peter@minskio.co.uk>
2025-12-29 15:59:25 +0000

    Extra steam bits

--- common/steam.nix
+++ common/steam.nix
@@ -1,3 +1,18 @@
+{ pkgs, ... }:
+
 {
 programs.steam.enable = true;
+programs.gamemode.enable = true;
+programs.steam.gamescopeSession.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-ng
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
 }
