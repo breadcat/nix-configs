fc95b9b963cec7eff4c4c9d015038121ebd944e0
breadcat <breadcat@users.noreply.github.com>
2026-05-28 15:22:25 +0100

    Parent programs prefix

--- common/steam.nix
+++ common/steam.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 
 {
-programs.steam.enable = true;
-programs.gamemode.enable = true;
-programs.steam.gamescopeSession.enable = true;
+  programs = {
+    gamemode.enable = true;
+    steam.enable = true;
+    steam.gamescopeSession.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     mangohud
