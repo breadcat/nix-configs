88adddc9010f3ed0aba813d3f783702aad921dc5
breadcat <breadcat@users.noreply.github.com>
2026-09-17 15:43:31 +0100

    These 500MB boot partitions are killing me

--- common/boot-systemd.nix
+++ common/boot-systemd.nix
@@ -1,7 +1,10 @@
 {
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+		enable = true;
+		configurationLimit = 3;
+	  };
       efi.canTouchEfiVariables = true;
     };
   };
